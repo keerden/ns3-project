7160821456be1a77e5167ed1d24f3020a8c0c161
Koos Eerden <keerden@users.noreply.github.com>
2016-04-28 22:38:07 +0200

    Cleaned up, only left the working last version

---
+++ sim-no-rts.sh
@@ -0,0 +1,16 @@
+
+
+output=inteference-norts.$(date +%m%d%Y_%H%M%S)
+maxa=16
+maxb=16
+echo "Running simulation:";
+for ((a=1; a <= maxa ; a++))
+do
+ for ((b=1; b <= maxb ; b++))
+ do
+	echo "a=$a b=$b"
+	./waf --run "interference --nStasB=$b  --nStasA=$a --raw --rtsCts=1024" >> $output
+
+ done
+done
+echo "Done!";
